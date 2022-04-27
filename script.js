cbf50bd5c5fae48f52dbb1f4e37edffd67c96684
DerriKhan <101285294+DerriKhan@users.noreply.github.com>
2022-04-27 14:05:42 -0700

    added user selectors

--- script.js
+++ script.js
@@ -1,60 +1,132 @@
-var password=document.getElementById("password");
+var password = document.getElementById("password");
 // const array_name = [item1, item2, ...];
-     const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-     const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-     const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-     const specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "~", "^", "_", "}", "{", "]"]
-     const appendix = []
-     //concatenate each string then restrict to var lenght value
+var appendix = [];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  ">",
+  "=",
+  "?",
+  "@",
+  "[",
+  "~",
+  "^",
+  "_",
+  "}",
+  "{",
+  "]",
+];
 
-function generatePassword()
 
-  //collect user specifications
-  {
-      
-    var upperAns = window.confirm("Should password include uppercase?")
-      if ( upperAns=true ) {
-        appendix.concat(upperCase)
-        console.log(appendix)
-      }
-      var lowerAns = window.confirm("Should password include lowercase?")
-      if ( lowerAns=true ) {
-        appendix.concat(lowerCase)
-        console.log(appendix)
-      }
-      var numAns = window.confirm("Should password include numbers?")
-      if ( numAns=true ) {
-        appendix.concat(numbers)
-        console.log(numbers)
-      }
-      var specAns = window.confirm("Should password include special characters?")
-      if ( specAns=true ) {
-        appendix.concat(specialChar)
-        console.log(appendix)
-      }
-  var length = Number(window.prompt("How many characters should the password be?"))
-  console.log(length)
 
-    if (8 < length < 128) {
-    alert("Thank you!")
+function generatePassword() {
+  var pwLength =
+  Number(prompt("Enter password length between 8 and 128 characters"));
+  if (8 <= pwLength && pwLength <= 128) {
+    console.log(pwLength);
+    if (confirm("Should password include uppercase?") == true) {
+      appendix.push(upperCase);
     }
-    else {
-      alert("Password Must Be Between 8 and 128 Characters")
+  
+    if (confirm("Should password include lowercase?") == true) {
+      appendix.push(lowerCase);
     }
-  }
-      
-  // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
-  var generateBtn = document.querySelector("#generate");
   
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    if (confirm("Should password include numbers?") == true) {
+      appendix.push(numbers);
+    }
   
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
+    if (confirm("Should password include special characters?") == true) {
+      appendix.push(specialChar);
+    }
+  } else {
+    alert("Password Must Be Between 8 and 128 Characters"); 
+    }
+};
+console.log(appendix);
+
+
+// Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
